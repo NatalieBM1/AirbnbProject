a781516a92ca1b31e9ab23a899ba33577a75c498
Juanportilla12 <jjportilla222@umariana.edu.co>
2025-09-29 22:10:29 -0500

    notificacion

--- frontend/src/pages/Notifications.jsx
+++ frontend/src/pages/Notifications.jsx
@@ -14,6 +14,7 @@ const Notifications = () => {
     }
   }, [isAuthenticated]);
 
+
   const fetchNotifications = async () => {
     try {
       setLoading(true);
@@ -149,5 +150,6 @@ const Notifications = () => {
     </div>
   );
 };
+
 
 export default Notifications;
