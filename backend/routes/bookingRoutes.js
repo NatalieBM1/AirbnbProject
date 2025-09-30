97488339ad6db21470d85d7f44a1bfdf5f20679e
NatalieBM1 <127702162+NatalieBM1@users.noreply.github.com>
2025-09-29 21:41:57 -0500

    Rutas booking

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -8,11 +8,13 @@ const {
 } = require('../controllers/bookingController');
 const authMiddleware = require('../middleware/authMiddleware');
 
+// Crear un router de Express
 const router = express.Router();
 
 // Todas las rutas de reservas requieren autenticación
 router.use(authMiddleware);
 
+// Definir las rutas para las reservas
 router.get('/', getAllBookings);
 router.get('/:id', getBookingById);
 router.post('/', createBooking);
